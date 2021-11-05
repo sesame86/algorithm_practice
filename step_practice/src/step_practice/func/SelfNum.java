7aa34caf07dbf84b33957a6dc7fcf69a72eb2308
yeeun kim <kyy8006@naver.com>
2021-11-05 23:49:55 +0900

    self number

---
+++ step_practice/src/step_practice/func/SelfNum.java
@@ -0,0 +1,26 @@
+package step_practice.func;
+
+public class SelfNum {
+	public static int selfNum(int n) {
+		int num = n;
+		while(n>0) {
+			num += n % 10;
+			n /= 10;
+		}
+		return num;
+	}
+	public static void main(String[] args) {
+		boolean[] notSelfNum = new boolean[10001];
+		for(int i=1; i <= 10000; ++i) {
+			int dn = selfNum(i);
+			if(dn <= 10000) {
+				notSelfNum[dn] = true;
+			}
+		}
+		for(int i=1; i<notSelfNum.length; ++i) {
+			if(!notSelfNum[i]) {
+				System.out.println(i);
+			}
+		}
+	}
+}
